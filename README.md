1fd967fd5e1ba046f37fb84cd67d5d393d16e475
Denis Tsoi <denistsoi@gmail.com>
2019-06-03 11:14:54 +0800

    Init Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tables
+
+```css
+header-font: Sriracha
+primary-font-family: Karma
+secondary-font-family: Martel normal
+```
---
+++ src/.vuepress/theme/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col h-full">
+    <Content class="flex-1 container mx-auto leading-normal"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Layout"
+};
+</script>
+
+<style lang="stylus">
+@import './styles/theme.styl';
+</style>
